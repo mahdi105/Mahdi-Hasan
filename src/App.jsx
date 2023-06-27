2e9ed2366e26e904003d9accda5dc7dac97f314f
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-06-28 01:04:08 +0600

    Added Profile card

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
+import { FaDownload, FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa6'
+import { FaFacebookF } from 'react-icons/fa'
 
 function App() {
     return (
         <>
-            <div className='bg-[#040718]'>
+            <div className='bg-[#040718] h-[100vh]'>
                 <div className='container mx-auto px-2 md:px-10 lg:px-16  py-8 lg:py-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-8'>
-                    <div className='col-span-2'>
+                    <div className='col-span-2 relative'>
                         <Navbar></Navbar>
                     </div>
                     <div className='col-span-7'>
                         <Outlet></Outlet>
                     </div>
                     <div className='col-span-3'>
-                        My Photo and Contact Info
+                        <div style={{ boxShadow: '0px -15px #9538f9a3 inset' }} className='p-4 rounded-lg bg-gradient-to-b from-[#9638F9] to-blue-700'>
+                            <div className='mb-3'>
+                                <img className='w-full h-[250px] rounded-lg object-cover' src="https://i.ibb.co/09PTpsg/Untitled-design.png" alt="" />
+                            </div>
+                            <div>
+                                <h2 style={{ fontFamily: 'Poppins' }} className='text-center text-white mb-1 font-bold text-2xl'>Mahdi Hasan</h2>
+                                <p style={{ fontFamily: 'Poppins' }} className='text-[13px] text-center text-white font-medium mb-5'>Full Stack Developer, MERN Stack Enthusiast</p>
+                                <div className='flex justify-center mb-5'>
+                                    <button className='flex justify-center items-center gap-1 py-2 px-3 rounded-full border border-gray-300 text-white text-[12px] font-semibold hover:bg-white hover:text-black hover:border-white transition-all duration-300'><FaDownload></FaDownload><span></span>Download Resume</button>
+                                </div>
+                                <div className='flex items-center justify-center gap-5 text-white mb-5'>
+                                    <Link to='' className='hover:text-black transition-all duration-300'><FaLinkedinIn></FaLinkedinIn></Link>
+                                    <Link to='' className='hover:text-black transition-all duration-300'><FaGithub></FaGithub></Link>
+                                    <Link to='' className='hover:text-black transition-all duration-300'><FaFacebookF></FaFacebookF></Link>
+                                    <Link to='' className='hover:text-black transition-all duration-300'><FaTwitter></FaTwitter></Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaGraduationCap, FaRegAddressBook, FaUser } from "react-icons/fa6";
+import { FaLaptopCode } from "react-icons/fa"
 import { HiOutlineBriefcase } from "react-icons/hi";
 import './Navbar.css'
 const Navbar = () => {
     const navigate = useNavigate();
     return (
-        <header className='flex flex-col items-center gap-16 h-[70vh] w-[70px] p-3 rounded-full border-2 border-gray-500'>
+        <header className='absolute top-0 left-0 flex flex-col items-center gap-16 h-[90vh] w-[70px] p-3 rounded-full border-2 border-gray-500'>
             <div className='flex justify-center items-center'>
                 <img onClick={() => navigate('/')} className='cursor-pointer w-[100%] border p-1 rounded-full shadow-sm bg-gray-700 shadow-white' src="https://i.ibb.co/xfVYZFF/letter-m-2.png" alt="Logo" />
             </div>
@@ -35,7 +36,17 @@ const Navbar = () => {
                         <span className='text-xl'>
                             <HiOutlineBriefcase></HiOutlineBriefcase>
                         </span>
-                        <span>Portfolio</span>
+                        <span>Projects</span>
+                    </span>
+                </Link>
+                <hr className="border-gray-700" />
+                <Link className='navItem relative text-[24px] rounded-full p-2 transition-all duration-200 text-gray-300' to='/skills'>
+                    <FaLaptopCode></FaLaptopCode>
+                    <span className='title absolute shadow-sm shadow-purple-400 top-0 -left-[11px] text-[12px] w-[130px] py-2 px-3 rounded-full bg-gradient-to-r from-blue-600 to-[#9638F9] font-bold uppercase flex justify-center items-center gap-1'>
+                        <span className='text-xl'>
+                            <FaLaptopCode></FaLaptopCode>
+                        </span>
+                        <span>Skills</span>
                     </span>
                 </Link>
                 <hr className="border-gray-700" />
