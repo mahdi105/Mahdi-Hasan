5314b7125b0f1d6ddcb160237a6210559854998b
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-07-03 16:56:53 +0600

    Fixed the dropdown menu

--- src/components/DropdownMenu/DropdownMenu.jsx
+++ src/components/DropdownMenu/DropdownMenu.jsx
@@ -14,7 +14,6 @@ const DropdownMenu = () => {
                         <li><Link to='/'>About</Link></li>
                         <li><Link to='/resume'>Resume</Link></li>
                         <li><Link to='/projects'>Projects</Link></li>
-                        <li><Link to='/skills'>Skills</Link></li>
                         <li><Link to='/contact'>Contact</Link></li>
                     </ul>
                 </div>
